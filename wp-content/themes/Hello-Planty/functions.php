3e4be127242734b038a72f2f69eba7b2c82befe5
fatimaghd <fatima.ghandi@hotmail.fr>
2023-10-14 10:08:15 +0200

    Ajout du formulaire de précommande

--- wp-content/themes/Hello-Planty/functions.php
+++ wp-content/themes/Hello-Planty/functions.php
@@ -5,3 +5,17 @@ function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');  
 }
+
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (!is_user_logged_in()) {
+        ?>
+        <style>
+            #menu-item-157 {
+                display: none !important;
+            }
+        </style>
+        <?php
+    }
+    return $items;
+}
